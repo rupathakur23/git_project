814d8d84e2de1c68228a5dba29e644ef7c13f90c
rupa <rupa@gmail.com>
2026-01-23 21:35:40 +0530

    dev-server

---
+++ ec2.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "us-east-1"   
+}
+
+resource "aws_instance" "my_instace" {
+    ami = "ami-0360c520857e3138f"
+    instance_type = "t2.micro"
+    tags = {
+        
+        Name = "dev-server"
+    }
+  
+}
